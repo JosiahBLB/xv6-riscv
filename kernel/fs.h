aa098c7fe4069351b3f741c1b9a7d7b7752adc18
JBrough <josiahbrough@proton.me>
2024-10-05 16:55:35 +1300

    Add include guards

--- kernel/fs.h
+++ kernel/fs.h
@@ -1,3 +1,8 @@
+#ifndef FS_H
+#define FS_H
+
+#include "types.h"
+
 // On-disk file system format.
 // Both the kernel and user programs use this header file.
 
@@ -58,3 +63,4 @@ struct dirent {
   char name[DIRSIZ];
 };
 
+#endif // !FS_H
